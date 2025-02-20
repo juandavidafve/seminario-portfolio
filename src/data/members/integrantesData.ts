23da62a1c3bffebc053664a3b8ca441e96a5d891
saimers <saimeradriansr@ufps.edu.co>
2025-02-20 09:50:05 -0500

    fix: add cards team

---
+++ src/data/members/integrantesData.ts
@@ -0,0 +1,58 @@
+export interface Member {
+    name: string;
+    code: string;
+    rol: string;
+    github: string;
+    user: string;
+    img: string;
+    description: string;
+  }
+  
+  export const members: Member[] = [
+    {
+      name: "Bryan Vera",   
+      code: "1152277",
+      rol: "Desarrollador Backend",
+      github: "https://github.com/BryanVeraDev",
+      user: "BryanVeraDev",
+      img: "/avatars/bryan.jpg",
+      description: "thebestbest",
+    },
+    {
+      name: "Saimer Saavedra",
+      code: "1152280",
+      rol: "Diseñador UX/UI",
+      github: "https://github.com/saimersaavedra",
+      user: "saimersaavedra",
+      img: "/avatars/saimer.jpg",
+      description: "best",
+    },
+    {
+      name: "Omar Jaimes",
+      code: "1152263",
+      rol: "Desarrollador Frontend",
+      github: "https://github.com/omarmolina23",
+      user: "OmarMolina23",
+      img: "/avatars/omar.jpg",
+      description: "ojoculos",
+    },
+    {
+        name: "Brayan Gonzalez",
+        code: "1152281",
+        rol: "Desarrollador Frontend, Tester",
+        github: "https://github.com/BSebastianGonzalez",
+        user: "BSebastianGonzalez",
+        img: "/avatars/sebastian.jpg",
+        description: "cabrito",
+    },
+    {
+        name: "Juan Afanador",
+        code: "1152247",
+        rol: "Desarrollador Backend, DBA",
+        github: "https://github.com/juandavidafve",
+        user: "juandavidafve",
+        img: "/avatars/juan.jpg",
+        description: "la cabra",
+    },
+  ];
+  
